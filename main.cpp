c3599f800ebb3f14d98c88292716c32641081d51
Cabbage McGravel <87149601+CabbageMcGravel@users.noreply.github.com>
2023-09-18 13:17:31 +0100

    Begin further parsing of blocks (world, solid, entity, group)
    -Add initial map reporting after parse

--- main.cpp
+++ main.cpp
@@ -13,17 +13,31 @@ enum class Tokens {
     Solid,
     Entity,
     Side,
+    World,
+    Group,
+    Version_Info,
+    Hidden,
+    Cameras,
+    Cordon,
+    Editor_Block,
 };
 
 struct Visgroup {
     const std::string name;
     size_t brush_count{};
     size_t entity_count{};
+    //TODO: add sub-visgroup support, perhaps with a visgroup variable?
+    // this ties into the VMF_File usage of std::vector<Visgroup> though.
+    // it is probably going to change to a different data type later -
+    // would that affect the data type of this struct? Can we still have a vector?
+    std::vector<Visgroup> sub_visgroups{};
 };
 
 struct VMF_File {
     size_t brush_count{};
     size_t side_count{};
+    size_t entity_count{};
+    //TODO: this vector may need to become a hash table in order to use the visgroup id as a key.
     std::vector<Visgroup> visgroups{};
 };
 
@@ -33,14 +47,22 @@ inline void update_depth(const std::string_view &line, size_t &depth) {
     if (line == "}") depth--;
 }
 
-Tokens evaluate_token(const std::string_view &line) {
+Tokens line_to_token(const std::string_view &line) {
     if (line == "{") return Tokens::Brace_Open;
     if (line == "}") return Tokens::Brace_Close;
+    if (line == "versioninfo") return Tokens::Version_Info;
     if (line == "visgroups") return Tokens::Visgroup_Block;
     if (line == "visgroup") return Tokens::Visgroup_Single;
+    if (line == "world") return Tokens::World;
     if (line == "solid") return Tokens::Solid;
     if (line == "entity") return Tokens::Entity;
     if (line == "side") return Tokens::Side;
+    if (line == "group") return Tokens::Group;
+    if (line == "hidden") return Tokens::Hidden;
+    if (line == "cameras") return Tokens::Cameras;
+    if (line == "cordon") return Tokens::Cordon;
+    if (line == "editor") return Tokens::Editor_Block;
+
     return Tokens::None;
 }
 
@@ -53,26 +75,83 @@ void parse_visgroup(VMF_File &vmf, std::ifstream &file, const size_t &return_dep
         update_depth(line, inner_depth);
         //TODO: figure out how to determine sub-visgroups (a visgroup inside a visgroup).
         // Considering using depth, or perhaps amount of tabs in the line.
-        if (evaluate_token(line) == Tokens::Visgroup_Single) {
-            size_t visgroup_depth{inner_depth};
-            do {
-                std::getline(file, line);
-                boost::trim_left(line);
-                update_depth(line, visgroup_depth);
+        // EDIT: after checking how the file is laid out, depth should work for us.
+        if (line_to_token(line) != Tokens::Visgroup_Single) continue;
+        size_t visgroup_depth{inner_depth};
+        do {
+            std::getline(file, line);
+            boost::trim_left(line);
+            update_depth(line, visgroup_depth);
 
-                std::vector<std::string> split_line;
-                split_line.reserve(3);
-                boost::split(split_line, line, [](const char &c) { return c == '"'; });
-                for (int i = 0; i < split_line.size(); ++i) {
-                    if (split_line[i] == "name") {
-                        Visgroup visgroup{.name = split_line[3]};
-                        vmf.visgroups.push_back(visgroup);
-                        break;
-                    }
+            std::vector<std::string> split_line;
+            split_line.reserve(3);
+            boost::split(split_line, line, [](const char &c) { return c == '"'; });
+            for (int i = 0; i < split_line.size(); ++i) {
+                if (split_line[i] == "name") {
+                    Visgroup visgroup{.name = split_line[3]};
+                    //TODO: further visgroup parsing, will have to be done outside of this function?
+                    // probably requires a hash map (unordered_map) in order to convert the visgroup ID into
+                    // a key for later access?
+                    vmf.visgroups.push_back(visgroup);
+                    break;
                 }
-            } while (visgroup_depth > inner_depth);
-        }
+            }
+        } while (visgroup_depth > inner_depth);
     } while (inner_depth > return_depth);
+}
+
+void parse_solid(VMF_File &vmf, std::ifstream &file, const size_t &return_depth) {
+    vmf.brush_count++;
+    size_t inner_depth{return_depth};
+    do {
+        std::string line;
+        std::getline(file, line);
+        boost::trim_left(line);
+        update_depth(line, inner_depth);
+        if (line_to_token(line) == Tokens::Side) vmf.side_count++;
+    } while (inner_depth > return_depth);
+}
+
+void parse_entity(VMF_File &vmf, std::ifstream &file, const size_t &return_depth) {
+    //TODO: is this where we check the visgroup id and add them to the appropriate visgroup's count?
+    vmf.entity_count++;
+}
+
+void parse_world_block(VMF_File &vmf, std::ifstream &file, const size_t &return_depth) {
+    size_t inner_depth{return_depth};
+    //Go through the 7 KeyValues of the world block
+    //TODO: do something with these values?
+    std::string line;
+    for (int i = 0; i < 7; ++i) {
+        std::getline(file, line);
+        boost::trim_left(line);
+        //TODO: This update_depth() only really updates once at the first line with the open brace,
+        // then never again. This is a known quantity since the file is presumably laid out the same.
+        // should I just update this manually instead? Pros? Cons? Overthinking it as usual?
+        update_depth(line, inner_depth);
+    }
+    //Afterward, we will begin parsing the Solid and, later on, the Group blocks inside the World block.
+    do {
+        std::getline(file, line);
+        boost::trim_left(line);
+        update_depth(line, inner_depth);
+        Tokens token{line_to_token(line)};
+        if (token == Tokens::Solid) parse_solid(vmf, file, return_depth);
+        //TODO: parse Group blocks
+        //if (token == Tokens::Group) continue;
+    } while (inner_depth > return_depth);
+}
+
+void map_report(const VMF_File &vmf) {
+    std::cout << '\t' << vmf.brush_count << " brushes\n";
+    std::cout << '\t' << vmf.side_count << " brush sides\n";
+    std::cout << '\t' << vmf.entity_count << " entities\n";
+    std::cout << '\t' << vmf.visgroups.size() << " visgroups:\n";
+
+    if (vmf.visgroups.empty()) return;
+    for (const auto &item: vmf.visgroups) {
+        std::cout << "\t\t" << item.name << '\n';
+    }
 }
 
 void parse_file(std::ifstream &current_vmf) {
@@ -80,7 +159,7 @@ void parse_file(std::ifstream &current_vmf) {
     size_t depth{0};
     for (std::string line; std::getline(current_vmf, line);) {
         boost::trim_left(line);
-        Tokens token{evaluate_token(line)};
+        Tokens token{line_to_token(line)};
         switch (token) {
             case Tokens::Brace_Open:
                 depth++;
@@ -91,18 +170,17 @@ void parse_file(std::ifstream &current_vmf) {
             case Tokens::Visgroup_Block:
                 parse_visgroup(map, current_vmf, depth);
                 break;
-            case Tokens::Visgroup_Single:
-                assert(false && "Individual visgroup detected outside of Visgroups block?");
-            case Tokens::Solid:
+            case Tokens::World:
+                parse_world_block(map, current_vmf, depth);
                 break;
             case Tokens::Entity:
-                break;
-            case Tokens::Side:
+                parse_entity(map, current_vmf, depth);
                 break;
             default:
                 break;
         }
     }
+    map_report(map);
 }
 
 int main(const int argc, const char **argv) {
